12d2532ff57795dd762dfd36a3d65178f3b7b01a
Roger <rogergonzalezacosta@gmail.com>
2025-03-10 21:11:49 +0100

    color theme changed

--- app/src/main/java/com/inovatech/smartpack/ui/theme/Color.kt
+++ app/src/main/java/com/inovatech/smartpack/ui/theme/Color.kt
@@ -1,5 +1,6 @@
 package com.inovatech.smartpack.ui.theme
 
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
@@ -9,3 +10,15 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val LoginScreenBackground = Brush.verticalGradient(
+    colors = listOf(Color(0xFFF6D051), Color(0xFFF3871A))
+)
+
+val OrangePrimary = Color(0xFFF3871A)
+val BlueSecondary = Color(0xFF1A73E8)
+val BlueTertiary = Color(0xFF004BA0)
+
+val DarkOrangePrimary = Color(0xFFF6D051)
+val DarkBlueSecondary = Color(0xFF1A73E8)
+val DarkBlueTertiary = Color(0xFF004BA0)
--- app/src/main/java/com/inovatech/smartpack/ui/theme/Theme.kt
+++ app/src/main/java/com/inovatech/smartpack/ui/theme/Theme.kt
@@ -13,15 +13,21 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+//    primary = Purple80,
+//    secondary = PurpleGrey80,
+//    tertiary = Pink80
+    primary = DarkOrangePrimary,
+    secondary = DarkBlueSecondary,
+    tertiary = DarkBlueTertiary
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40,
+//    primary = Purple40,
+//    secondary = PurpleGrey40,
+//    tertiary = Pink40,
+    primary = OrangePrimary,
+    secondary = BlueSecondary,
+    tertiary = BlueTertiary,
 
     // Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -32,14 +38,14 @@ private val LightColorScheme = lightColorScheme(
     onBackground = Color(0xFF1C1B1F),
     onSurface = Color(0xFF1C1B1F),
 
-)
+    )
 
 @Composable
 fun SmartPackTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit,
 ) {
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
