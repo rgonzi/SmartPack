4dfe2c2849b5aa7062cb8896104f7fa6819233e6
Roger <rogergonzalezacosta@gmail.com>
2025-03-10 11:36:03 +0100

    init NavGraph and LoginScreen

--- app/src/main/java/com/inovatech/smartpack/ui/Navigation.kt
+++ app/src/main/java/com/inovatech/smartpack/ui/Navigation.kt
@@ -2,6 +2,8 @@ package com.inovatech.smartpack.ui
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
@@ -9,20 +11,23 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.inovatech.smartpack.ui.screens.Login
+import com.inovatech.smartpack.ui.screens.LoginScreen
 
 @Composable
-fun Navigation (
-    modifier: Modifier = Modifier
+fun Navigation(
+    modifier: Modifier = Modifier,
 ) {
     val navController: NavHostController = rememberNavController()
 
-    NavHost(
-        navController = navController,
-        startDestination = Login,
-        modifier = modifier.fillMaxSize()
-    ) {
-        composable<Login> {
-
+    Scaffold {
+        NavHost(
+            navController = navController,
+            startDestination = Login,
+            modifier = modifier.fillMaxSize().padding(it)
+        ) {
+            composable<Login> {
+                LoginScreen()
+            }
         }
     }
 }
--- app/src/main/java/com/inovatech/smartpack/ui/screens/Login.kt
+++ app/src/main/java/com/inovatech/smartpack/ui/screens/Login.kt
@@ -1,9 +1,56 @@
 package com.inovatech.smartpack.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import kotlinx.serialization.Serializable
+import com.inovatech.smartpack.R
 
 @Serializable
 object Login
 
 @Composable
+fun LoginScreen() {
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Spacer(modifier = Modifier.weight(1f))
+        Image(
+            painter = painterResource(id = R.drawable.logo_blanc),
+            contentDescription = "Logo SmartPack",
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        //Email
+        Spacer(modifier = Modifier.weight(1f))
+        //Password
+        Spacer(modifier = Modifier.weight(1f))
+        Button(
+            modifier = Modifier.padding(16.dp),
+            onClick = { /*TODO*/ }
+        ) {
+            Text("Iniciar sessió")
+        }
+        Button(
+            modifier = Modifier.padding(16.dp),
+            onClick = { /*TODO*/ }
+        ) {
+            Text("Registrar-se")
+        }
+        Spacer(modifier = Modifier.weight(1f))
+    }
+}
+
+@Preview
+@Composable
+fun LoginScreenPreview() {
+    LoginScreen()
+}
