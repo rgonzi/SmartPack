54bfc5f36b7f24f39d2cc39cad3cd060528263c3
Roger <rogergonzalezacosta@gmail.com>
2025-03-11 09:48:00 +0100

    LoginScreen improvement, added good icon

--- app/src/main/java/com/inovatech/smartpack/ui/Navigation.kt
+++ app/src/main/java/com/inovatech/smartpack/ui/Navigation.kt
@@ -4,8 +4,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -20,13 +22,11 @@ fun Navigation(
 ) {
     val navController: NavHostController = rememberNavController()
 
-    Scaffold (
-        modifier = Modifier.fillMaxSize()
-    ){
+    Surface {
         NavHost(
             navController = navController,
             startDestination = Login,
-            modifier = modifier.fillMaxSize().padding(it)
+            modifier = modifier.fillMaxSize()
         ) {
             composable<Login> {
                 LoginScreen()
--- app/src/main/java/com/inovatech/smartpack/ui/screens/Login.kt
+++ app/src/main/java/com/inovatech/smartpack/ui/screens/Login.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
@@ -14,14 +13,16 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.*
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.google.firebase.annotations.concurrent.Background
+import androidx.compose.ui.unit.sp
 import kotlinx.serialization.Serializable
 import com.inovatech.smartpack.R
 import com.inovatech.smartpack.ui.theme.LoginScreenBackground
@@ -35,95 +36,111 @@ fun LoginScreen() {
         modifier = Modifier
             .fillMaxSize()
             .background(LoginScreenBackground)
-            .padding(vertical = 32.dp, horizontal = 64.dp),
+            .padding(32.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Spacer(modifier = Modifier.weight(1f))
         Image(
-            painter = painterResource(id = R.drawable.logo_blanc),
+            painter = painterResource(id = R.drawable.logo_transparent),
             contentDescription = "Logo SmartPack",
+            contentScale = ContentScale.FillWidth,
             modifier = Modifier.fillMaxWidth()
         )
-        Spacer(modifier = Modifier.weight(0.5f))
+        Spacer(modifier = Modifier.weight(1f))
 
-        //Email
-        OutlinedTextField(
-            colors = TextFieldDefaults.colors(
-                focusedContainerColor = Color.White,
-                unfocusedContainerColor = Color.White
-            ),
-            modifier = Modifier.fillMaxWidth(),
-            value = "",
-            onValueChange = {},
-            singleLine = true,
-            label = { Text("Correu") },
-            trailingIcon = {
-                Icon(
-                    imageVector = Icons.Default.Email,
-                    contentDescription = "Icona d'un email"
-                )
-            },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Email,
-                imeAction = ImeAction.Next
+        Column (
+            modifier = Modifier.fillMaxWidth()
+                .padding(horizontal = 32.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Text(
+                "Inicia sessió",
+                fontWeight = FontWeight.Bold,
+                fontSize = 32.sp
             )
-        )
+            Spacer(modifier = Modifier.height(16.dp))
 
-        Spacer(modifier = Modifier.height(8.dp))
-
-        //Password
-        OutlinedTextField(
-            colors = TextFieldDefaults.colors(
-                focusedContainerColor = Color.White,
-                unfocusedContainerColor = Color.White
-            ),
-            modifier = Modifier.fillMaxWidth(),
-            value = "",
-            onValueChange = {},
-            singleLine = true,
-            label = { Text("Contrassenya") },
-            visualTransformation = PasswordVisualTransformation(), /* TODO toggle password visibility */
-            trailingIcon = {
-                Icon(
-                    imageVector = Icons.Default.Lock,
-                    contentDescription = "Icona d'un candau"
-                )
-            },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Password,
-                imeAction = ImeAction.Done
-            )
-        )
-        Text(
-            "Has oblidat la contrassenya?",
-            modifier = Modifier
-                .align(Alignment.End)
-                .clickable {
-                    /*TODO Anar a recordar contrassenya */
+            //Email
+            OutlinedTextField(
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color.White,
+                    unfocusedContainerColor = Color.White
+                ),
+                modifier = Modifier.fillMaxWidth(),
+                value = "",
+                onValueChange = {},
+                singleLine = true,
+                label = { Text("Correu") },
+                trailingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Email,
+                        contentDescription = "Icona d'un email"
+                    )
                 },
-            fontStyle = FontStyle.Italic
-        )
-        Spacer(modifier = Modifier.weight(0.5f))
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Email,
+                    imeAction = ImeAction.Next
+                )
+            )
 
-        Button(
-            modifier = Modifier.fillMaxWidth(),
-            onClick = { /*TODO*/ }
-        ) {
-            Text("Iniciar sessió")
-        }
-        Spacer(modifier = Modifier.height(8.dp))
+            Spacer(modifier = Modifier.height(8.dp))
 
-        Button(
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color.White,
-                contentColor = Color.Black
-            ),
-            modifier = Modifier.fillMaxWidth(),
-            onClick = { /*TODO*/ }
-        ) {
-            Text("Registrar-me")
+            //Password
+            OutlinedTextField(
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color.White,
+                    unfocusedContainerColor = Color.White
+                ),
+                modifier = Modifier.fillMaxWidth(),
+                value = "",
+                onValueChange = {},
+                singleLine = true,
+                label = { Text("Contrasenya") },
+                visualTransformation = PasswordVisualTransformation(), /* TODO toggle password visibility */
+                trailingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Lock,
+                        contentDescription = "Icona d'un candau"
+                    )
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password,
+                    imeAction = ImeAction.Done
+                )
+            )
+
+            Text(
+                "He oblidat la contrassenya",
+                modifier = Modifier
+                    .align(Alignment.End)
+                    .clickable {
+                        /*TODO Anar a recordar contrasenya */
+                    },
+                fontWeight = FontWeight.Bold,
+                textDecoration = TextDecoration.Underline
+            )
+            Spacer(modifier = Modifier.weight(0.5f))
+
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = { /*TODO Inici de sessió*/ }
+            ) {
+                Text("Iniciar sessió", fontWeight = FontWeight.Bold)
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Button(
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.DarkGray,
+                    contentColor = Color.White
+                ),
+                modifier = Modifier.fillMaxWidth(),
+                onClick = { /*TODO Registre*/ }
+            ) {
+                Text("Registra't")
+            }
+            Spacer(modifier = Modifier.weight(2f))
         }
-        Spacer(modifier = Modifier.weight(2f))
     }
 }
 
