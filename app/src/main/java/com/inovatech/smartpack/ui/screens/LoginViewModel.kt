1ef605fbc8939c289bdbb6994b5f6e388211c0d9
Roger <rogergonzalezacosta@gmail.com>
2025-03-12 16:50:04 +0100

    implemented login func

--- app/src/main/java/com/inovatech/smartpack/ui/screens/LoginViewModel.kt
+++ app/src/main/java/com/inovatech/smartpack/ui/screens/LoginViewModel.kt
@@ -1,10 +1,15 @@
 package com.inovatech.smartpack.ui.screens
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.inovatech.smartpack.model.LoginUiState
+import com.inovatech.smartpack.network.RetrofitInstance
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import okio.IOException
+import retrofit2.HttpException
 
-class LoginViewModel: ViewModel() {
+class LoginViewModel : ViewModel() {
     private val _uiState = MutableStateFlow(LoginUiState())
     val uiState: StateFlow<LoginUiState> = _uiState.asStateFlow()
 
@@ -23,6 +28,48 @@ class LoginViewModel: ViewModel() {
     fun togglePasswordVisibility() {
         _uiState.update {
             it.copy(passwordVisible = !it.passwordVisible)
+        }
+    }
+
+    fun login() {
+        viewModelScope.launch {
+            _uiState.update {
+                it.copy(isLoading = true, error = null)
+            }
+            try {
+                val email = _uiState.value.email
+                val password = _uiState.value.password
+
+                //TODO Validar camps amb regex
+
+                if (email.isNotEmpty() && password.isNotEmpty()) {
+                    val response = RetrofitInstance.smartPackRepository.login(
+                        email = email, password = password
+                    )
+                    if (response.isSuccessful) {
+                        val loginResponse = response.body()
+                        _uiState.update {
+                            it.copy(token = loginResponse?.token, error = null)
+                        }
+                    } else {
+                        _uiState.update {
+                            it.copy(error = "S'ha produït un error: ${response.code()}")
+                        }
+                    }
+                }
+            } catch (e: HttpException) {
+                _uiState.update {
+                    it.copy(error = "S'ha produït un error: ${e.message()}")
+                }
+            } catch (e: IOException) {
+                _uiState.update {
+                    it.copy(error = "S'ha produït un error de connexió: ${e.message}")
+                }
+            } finally {
+                _uiState.update {
+                    it.copy(isLoading = false)
+                }
+            }
         }
     }
 }
