44828961dc73e295068547ae5162ad317f2578d2
Roger <rogergonzalezacosta@gmail.com>
2025-03-19 12:43:48 +0100

    implemented signup fun

--- app/src/main/java/com/inovatech/smartpack/ui/screens/SignUpViewModel.kt
+++ app/src/main/java/com/inovatech/smartpack/ui/screens/SignUpViewModel.kt
@@ -2,17 +2,25 @@ package com.inovatech.smartpack.ui.screens
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.inovatech.smartpack.data.SmartPackRepository
+import com.inovatech.smartpack.model.RegisterRequest
 import com.inovatech.smartpack.model.SignUpUiState
 import com.inovatech.smartpack.model.Usuari
+import com.inovatech.smartpack.utils.Settings.TIMEOUT
 import com.inovatech.smartpack.utils.isValidEmail
 import com.inovatech.smartpack.utils.isValidPassword
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeoutOrNull
+import okio.IOException
 import javax.inject.Inject
 
 @HiltViewModel
-class SignUpViewModel @Inject constructor() : ViewModel() {
+class SignUpViewModel @Inject constructor(
+    private val smartPackRepository: SmartPackRepository,
+) : ViewModel() {
     private val _uiState = MutableStateFlow(SignUpUiState())
     val uiState: StateFlow<SignUpUiState> = _uiState.asStateFlow()
 
@@ -48,10 +56,39 @@ class SignUpViewModel @Inject constructor() : ViewModel() {
 
         _uiState.update { it.copy(isLoading = true) }
 
-        val usuariPerRegistrar = Usuari(email = state.email, password = state.password)
+        val usuariPerRegistrar = RegisterRequest(
+            email = state.email,
+            password = state.password
+        )
 
         viewModelScope.launch {
-            //TODO Registrar usuari. Veure LoginViewModel
+            delay(800)
+            val result = withTimeoutOrNull(TIMEOUT) {
+                try {
+                    val response = smartPackRepository.register(usuariPerRegistrar)
+
+                    if (response.isSuccessful && response.body() != null) {
+                        _uiState.update {
+                            it.copy(signUpSuccess = true, error = null)
+                        }
+                    } else {
+                        _uiState.update {
+                            it.copy(error = "S'ha produït un error: ${response.code()}")
+                        }
+                    }
+
+                } catch (e: IOException) {
+                    _uiState.update {
+                        it.copy(error = "S'ha produït un error en la petició: ${e.message}")
+                    }
+                }
+            }
+            if (result == null) {
+                _uiState.update {
+                    it.copy(error = "S'ha produït un error: S'ha superat el temps de resposta")
+                }
+            }
+            _uiState.update { it.copy(isLoading = false) }
         }
     }
 
