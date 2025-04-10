93c2fab11b600690e9370c2198eb9b722d5bdbee
Roger <rogergonzalezacosta@gmail.com>
2025-04-10 17:25:03 +0200

    theme forced always to Light Theme

--- app/src/main/java/com/inovatech/smartpack/ui/theme/Theme.kt
+++ app/src/main/java/com/inovatech/smartpack/ui/theme/Theme.kt
@@ -44,7 +44,8 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun SmartPackTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    //darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = false,
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit,
