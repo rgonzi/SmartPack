b1d79cbb5d6a265539d9e74d6db94dc1987697d8
Roger <rogergonzalezacosta@gmail.com>
2025-04-30 16:27:10 +0200

    modified minSdk to 26 (Android O)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
 
     defaultConfig {
         applicationId = "com.inovatech.smartpack"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
