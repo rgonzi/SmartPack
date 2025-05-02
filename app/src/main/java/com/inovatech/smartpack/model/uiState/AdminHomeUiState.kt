05ea4851aec8299d575be90783f69311711ee6c2
Roger <rogergonzalezacosta@gmail.com>
2025-05-02 20:25:41 +0200

    Revert "created Tabs for AdminHome"
    This reverts commit 6130b27204eb96aaa6bf1ab1bbb5e3a10186471f.

--- app/src/main/java/com/inovatech/smartpack/model/uiState/AdminHomeUiState.kt
+++ app/src/main/java/com/inovatech/smartpack/model/uiState/AdminHomeUiState.kt
@@ -6,17 +6,6 @@ import com.inovatech.smartpack.model.User
  * Classe que defineix l'estat de la UI de la pantalla d'inici de l'usuari administrador.
  */
 data class AdminHomeUiState(
-    val selectedUser: User? = null,
-    val usersList: List<User> = emptyList(),
-    val isLoading: Boolean = false,
-    val isRefreshing: Boolean = false,
-    val searchQuery: String = "",
-    val msg: String? = null,
-) {
-    val filteredUsers: List<User>
-        get() = usersList.filter {
-            (searchQuery.isBlank() || it.name!!.contains(searchQuery, true) || it.email!!.contains(
-                searchQuery, true
-            ))
-        }
-}
+    val user: User? = null,
+    val isLogoutSuccess: Boolean = false
+)
